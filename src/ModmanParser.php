621269a8bc1735be2ffdb00eaa05d976386aaf26
Vinai Kopp <vinai@netzarbeiter.com>
2012-10-27 14:09:37 +0200

    First untested commit of modman file parser

--- src/ModmanParser.php
+++ src/ModmanParser.php
@@ -38,13 +38,43 @@ class ModmanParser
         return $this->_composer->getConfig()->get('vendor-dir') . DIRECTORY_SEPARATOR . 'modman';
     }
 
-    public function parseModman($file = null)
+    /**
+     * @param string $file
+     * @return array
+     * @throws \ErrorException
+     */
+    public function getMappings($file = null)
     {
         if (null === $file) {
             $file = $this->getFile();
         }
         if (! is_readable($file)) {
-            throw new
+            throw new \ErrorException(sprintf('modman file "%s" not readable', $file));
         }
+        $map = $this->_parseMappings(file_get_contents($file));
+        return $map;
+    }
+
+    /**
+     * @param $modmanData
+     * @return array
+     */
+    protected function _parseMappings($modmanData)
+    {
+        $map = array();
+        $line = 0;
+        foreach (explode("\n", $modmanData) as $row) {
+            $line++;
+            $row = trim($row);
+            if ('' === $row || in_array($row{0}, array('#', '@'))) {
+                continue;
+            }
+            $parts = preg_split('/\s+/', $row, 2, PREG_SPLIT_NO_EMPTY);
+            if (count($parts) != 2) {
+                throw new \ErrorException(sprintf('Invalid row on line %d has %d parts, expected 2', $line, count($row));
+            }
+            $map[$parts[0]] = $parts[1];
+        }
+        return $map;
     }
 }
