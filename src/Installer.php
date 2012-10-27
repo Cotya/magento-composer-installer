c6edf78fc32912cad977407f6b2e52e365a99fec
Vinai Kopp <vinai@netzarbeiter.com>
2012-10-27 14:17:18 +0200

    Add todo's for missing modman target implementations

--- src/Installer.php
+++ src/Installer.php
@@ -141,6 +141,8 @@ class Installer extends \Composer\Installer\LibraryInstaller implements \Compose
      * @param $source
      * @param $dest
      * @throws \ErrorException
+     * @todo implement file to dir modman target, e.g. Namespace_Module.csv => app/locale/de_DE/
+     * @todo implement glob to dir mapping target, e.g. code/* => app/code/local/
      */
     protected function _createSymlink($source, $dest)
     {
