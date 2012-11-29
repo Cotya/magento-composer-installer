2b1c19e396df15479afd56ba293d608a83f99328
Vinai Kopp <vinai@netzarbeiter.com>
2012-11-29 15:38:23 +0100

    Add more tests

--- tests/MagentoHackathon/Composer/Magento/Deploystrategy/CopyTest.php
+++ tests/MagentoHackathon/Composer/Magento/Deploystrategy/CopyTest.php
@@ -38,7 +38,57 @@ class CopyTest extends AbstractTest
         touch($this->sourceDir . DIRECTORY_SEPARATOR . $src . DIRECTORY_SEPARATOR . "local.xml");
         $this->assertTrue(is_readable($this->sourceDir . DIRECTORY_SEPARATOR . $src . DIRECTORY_SEPARATOR . "local.xml"));
         $this->assertFalse(is_readable($this->destDir . DIRECTORY_SEPARATOR . $dest . DIRECTORY_SEPARATOR . "local.xml"));
-        $this->strategy->create($src,$dest);
+        $this->strategy->create($src, $dest);
         $this->assertTrue(is_readable($this->destDir . DIRECTORY_SEPARATOR . $dest . DIRECTORY_SEPARATOR . "local.xml"));
+    }
+
+    public function testGlobLink()
+    {
+        $glob_source = "modules/test.xml";
+        mkdir($this->sourceDir . dirname(DIRECTORY_SEPARATOR . $glob_source));
+        touch($this->sourceDir . DIRECTORY_SEPARATOR . $glob_source);
+
+        $glob_dest = "modules";
+        mkdir($this->destDir . DIRECTORY_SEPARATOR . $glob_dest);
+
+        $this->strategy->create($glob_source, $glob_dest);
+
+        $this->assertFileExists($this->destDir . DIRECTORY_SEPARATOR . $glob_dest);
+    }
+
+    public function testGlobLinkSlash()
+    {
+        $glob_source = "modules/test.xml";
+        mkdir($this->sourceDir . dirname(DIRECTORY_SEPARATOR . $glob_source));
+        touch($this->sourceDir . DIRECTORY_SEPARATOR . $glob_source);
+
+        $glob_dest = "modules/";
+
+        // first create will create file
+        $this->strategy->create($glob_source, $glob_dest);
+
+        // second create has to identify file
+        $this->strategy->create($glob_source, $glob_dest);
+
+        $this->assertFileExists($this->destDir . DIRECTORY_SEPARATOR . $glob_dest);
+    }
+
+    public function testGlobLinkWildcard()
+    {
+        $glob_source = "modules/*";
+        $glob_dir = dirname($glob_source);
+        $files = array('test1.xml', 'test2.xml');
+        mkdir($this->sourceDir . DIRECTORY_SEPARATOR . $glob_dir);
+        foreach ($files as $file) {
+            touch($this->sourceDir . DIRECTORY_SEPARATOR . $glob_dir . DIRECTORY_SEPARATOR . $file);
+        }
+
+        $glob_dest = "modules/";
+
+        $this->strategy->create($glob_source, $glob_dest);
+
+        foreach ($files as $file) {
+            $this->assertFileExists($this->destDir . DIRECTORY_SEPARATOR . $glob_dest . DIRECTORY_SEPARATOR . $file);
+        }
     }
 }
