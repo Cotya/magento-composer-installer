38c3ac93b121f8ab2b74307fbafab7a154c499c6
Flyingmana <flyingmana@googlemail.com>
2014-04-18 18:36:28 +0200

    change Installer to an actual Plugin

---
+++ src/MagentoHackathon/Composer/Magento/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+namespace MagentoHackathon\Composer\Magento;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface{
+
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+} 
