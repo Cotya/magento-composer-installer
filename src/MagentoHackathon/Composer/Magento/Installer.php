adac5b4bad6f9f5bfb54edee3b5b9128b4ae79f9
tchavoshinia <amir.tchavoshinia@brandung.de>
2014-05-25 06:52:27 +0200

    fixed default root dir value, added factory

--- src/MagentoHackathon/Composer/Magento/Installer.php
+++ src/MagentoHackathon/Composer/Magento/Installer.php
@@ -8,6 +8,7 @@ namespace MagentoHackathon\Composer\Magento;
 use Composer\Repository\InstalledRepositoryInterface;
 use Composer\IO\IOInterface;
 use Composer\Composer;
+use Composer\Factory;
 use Composer\Installer\LibraryInstaller;
 use Composer\Installer\InstallerInterface;
 use Composer\Package\PackageInterface;
@@ -23,6 +24,13 @@ class Installer extends LibraryInstaller implements InstallerInterface
      * @var \SplFileInfo
      */
     protected $magentoRootDir = null;
+
+    /**
+     * The default base directory of the magento installation
+     *
+     * @var \SplFileInfo
+     */
+    protected $defaultMagentoRootDir = 'root';
 
     /**
      * The base directory of the modman packages
@@ -76,7 +84,7 @@ class Installer extends LibraryInstaller implements InstallerInterface
 
         $extra = $composer->getPackage()->getExtra();
 
-        if (isset($extra['magento-root-dir']) || ($rootDirInput = $io->ask('please define your magento root dir' . 'root'))) {
+        if (isset($extra['magento-root-dir']) || ($rootDirInput = $io->ask('please define your magento root dir ', $this->defaultMagentoRootDir))) {
 
             if (isset($rootDirInput)) {
                 $extra['magento-root-dir'] = $rootDirInput;
@@ -140,6 +148,13 @@ class Installer extends LibraryInstaller implements InstallerInterface
         // $this->getSourceDir($package);
     }
 
+
+    /**
+     *
+     * @param array $extra
+     * @param \Composer\IO\IOInterface $io
+     * @return int
+     */
     private function updateJsonExtra($extra, $io) {
 
         $file = Factory::getComposerFile();
