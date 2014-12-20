213a556b7696d8be24c2ab0737379697499b67a2
Flyingmana <flyingmana@googlemail.com>
2014-12-20 04:16:09 +0100

    fix autoloading for executable

--- bin/magento-composer-installer.php
+++ bin/magento-composer-installer.php
@@ -3,6 +3,8 @@
 
 if (file_exists(__DIR__.'/../autoload.php')) {
     include __DIR__.'/../autoload.php';
+} elseif (file_exists(__DIR__.'/../../../autoload.php')) {
+    include __DIR__.'/../../../autoload.php';
 } elseif (file_exists(__DIR__.'/../vendor/autoload.php')) {
     include __DIR__.'/../vendor/autoload.php';
 } else {
