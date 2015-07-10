c37076a2225f5447bc0a56a9d3c34da71be56660
Michael A. Smith <msmith3@ebay.com>
2015-07-10 11:16:26 -0400

    Correct Method Spelling/Semantics

--- src/MagentoHackathon/Composer/Magento/Plugin.php
+++ src/MagentoHackathon/Composer/Magento/Plugin.php
@@ -149,7 +149,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $this->filesystem = new Filesystem();
         $this->config = new ProjectConfig($composer->getPackage()->getExtra(), $composer->getConfig()->all());
 
-        $this->veryfiyComposerRepositories();
+        $this->suggestComposerRepositories();
 
         $this->entryFactory = new EntryFactory(
             $this->config,
@@ -241,7 +241,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     /**
      * test configured repositories and give message about adding recommended ones
      */
-    protected function veryfiyComposerRepositories()
+    protected function suggestComposerRepositories()
     {
         $foundFiregento = false;
         $foundMagento   = false;
