9f4a0e980508fe1e76c589d39dad682431af3806
Flyingmana <flyingmana@googlemail.com>
2016-05-09 21:45:27 +0200

    try a fallback based composer install approach for travis

--- tests/prepare_composer.php
+++ tests/prepare_composer.php
@@ -1,13 +1,18 @@
 <?php
 copy('https://getcomposer.org/installer', 'composer-setup.php');
 echo hash_file('SHA384', 'composer-setup.php').PHP_EOL;
-if (getenv('COMPOSER_VERSION') === 'beta') {
-    passthru('php composer-setup.php --preview=beta');
-} elseif (getenv('COMPOSER_VERSION') === 'rc') {
-    passthru('php composer-setup.php --preview=rc');
-} elseif (getenv('COMPOSER_VERSION') === 'dev') {
-    passthru('php composer-setup.php --snapshot');
-} else {
-    passthru('php composer-setup.php --version='.getenv('COMPOSER_VERSION'));
+$command = 'php composer-setup.php';
+if (getenv('COMPOSER_VERSION')) {
+    if (getenv('COMPOSER_VERSION') === 'beta') {
+        $command = ('php composer-setup.php --preview=beta');
+    } elseif (getenv('COMPOSER_VERSION') === 'rc') {
+        $command = ('php composer-setup.php --preview=rc');
+    } elseif (getenv('COMPOSER_VERSION') === 'dev') {
+        $command = ('php composer-setup.php --snapshot');
+    } else {
+        $command = ('php composer-setup.php --version='.getenv('COMPOSER_VERSION'));
+    }
 }
+echo "execute: $command" . PHP_EOL;
+passthru($command);
 unlink('composer-setup.php');
